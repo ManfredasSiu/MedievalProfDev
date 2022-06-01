8a2973951ca6a843a6096e3e9b1463b81155ca77
Jokūbas Kondrackas <jokis125@gmail.com>
2022-06-01 17:46:11 +0300

    few fixes to UI

--- Assets/Scripts/GameResources/GameResources.cs
+++ Assets/Scripts/GameResources/GameResources.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class GameResources : MonoBehaviour
 {
-    public static List<GameResource> GAME_RESOURCES = new List<GameResource>()
+    public static Dictionary<string, GameResource> GAME_RESOURCES = new Dictionary<string, GameResource>()
     {
-        new GameResource("Something1", 1000),
-        new GameResource("Something2", 1000),
-        new GameResource("Something3", 1000),
-        new GameResource("Something4", 1000),
-        new GameResource("Something5", 1000),
-        new GameResource("Something6", 1000),
-        
+        {"resource1",new GameResource("Something1", 1000)},
+        {"resource2",new GameResource("Something1", 1000)},
+        {"resource3",new GameResource("Something1", 1000)},
     };
 }
