2b2ee94b8e540943411383443beec2d74a6b03bf
Jokūbas Kondrackas <jokis125@gmail.com>
2022-06-13 16:50:03 +0300

    expanded building hierarchy

--- Assets/Scripts/Units/Buildings/WorkerBuildings/StockpileBuilding.cs
+++ Assets/Scripts/Units/Buildings/WorkerBuildings/StockpileBuilding.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StockpileBuilding : Building
+public class StockpileBuilding : WorkerBuilding
 {
     public StockpileBuilding(BuildingData data) : base(data)
     {
